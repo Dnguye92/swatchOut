6e87ffdc00bb165195cbc447cd944131660d7d39
Dylan <Dnguye92@yahoo.com>
2017-03-01 16:42:27 -0800

    added slick.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 //Create functions for menu button
 let menuButton = document.getElementById('menuButton');
+let closeButton = document.getElementById('closeButton');
 let sideNav = document.getElementById('sideNav');
+
 menuButton.addEventListener('click', () => {
-	if(sideNav.style.width = '0') {
-		sideNav.style.width = '25em';
-		menuButton.textContent = 'X';
-	} else if(sideNav.style.width = '25em') {
-		sideNav.style.width = '0';
-		menuButton.textContent = 'Menu';
-	}
+	sideNav.style.width = '25em';
+})
+closeButton.addEventListener('click', () => {
+	sideNav.style.width = '0';
 })
