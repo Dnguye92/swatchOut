7be40fc46c8fc56e3330348153faf073e86f5dc4
Dylan <Dnguye92@yahoo.com>
2017-03-12 23:33:52 -0700

    adding array for watch data

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@
 let menuButton = document.getElementById('menuButton');
 let closeButton = document.getElementById('closeButton');
 let sideNav = document.getElementById('sideNav');
+let swatches = [
+	{
+		
+	}
+]
 
 menuButton.addEventListener('click', () => {
 	sideNav.style.width = '25em';
-})
+});
 closeButton.addEventListener('click', () => {
 	sideNav.style.width = '0';
-})
+});
+
