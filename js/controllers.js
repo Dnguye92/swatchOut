f2a9c5db0b892312bf265ae9b43cc29041bfa63f
Dylan <Dnguye92@yahoo.com>
2017-04-11 22:21:18 -0700

    split about section, still building controller for modal

--- js/controllers.js
+++ js/controllers.js
@@ -2,9 +2,9 @@ var watchControllers = angular.module('watchControllers', [
 	'ngAnimate'
 ])
 
-var modalControllers = angular.module('modalControllers', [
-	'ui.bootstrap'
-])
+// var modalControllers = angular.module('modalControllers', [
+// 	'ui.bootstrap'
+// ])
 
 watchControllers.controller('controller', ['$scope', '$http', function($scope, $http) {
 	$http.get('js/data.json').success(function(data) {
